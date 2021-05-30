ad6f4463aa1718787c3e5dc9b93f136d976064a5
Kevin Esler <kevin.esler@gmail.com>
2021-05-31 08:44:13 +1000

    Added a test using assert.

--- test/Main.purs
+++ test/Main.purs
@@ -3,7 +3,9 @@ module Test.Main where
 import Prelude
 import Effect (Effect)
 import Effect.Console (log)
+import Test.Assert (assert)
 
 main :: Effect Unit
 main = do
+  assert( 9 + 8 == 17)
   log "You should add some tests."
