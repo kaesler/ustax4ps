494fc0d1870bb2510e475f45ab9b58ee52edf69d
Kevin Esler <kevin.esler@gmail.com>
2021-12-02 18:22:46 -0500

    unsafeReadRegime

--- src/Federal/Regime.purs
+++ src/Federal/Regime.purs
@@ -2,7 +2,8 @@ module Federal.Regime
   ( Regime (..),
     invalidRegime,
     lastYearKnown,
-    requireRegimeValidInYear
+    requireRegimeValidInYear,
+    unsafeReadRegime
   )
 where
 
@@ -10,7 +11,10 @@ import Data.Enum
 
 import Data.Date (Year)
 import Data.Interpolate (i)
+import Data.Maybe (Maybe(..), fromJust)
+import Data.String.Read (class Read, read)
 import Effect.Exception.Unsafe (unsafeThrow)
+import Partial.Unsafe (unsafePartial)
 import Prelude (class Eq, class Ord, class Show, Unit, show, unit, ($), (<), (>), (>=), (||))
 import UnsafeDates (unsafeMakeYear)
 
@@ -20,6 +24,15 @@ derive instance Ord Regime
 instance Show Regime where
   show Trump = " Trump"
   show NonTrump = "NonTrump"
+instance Read Regime where
+  read s = 
+    case s of 
+      "Trump" -> Just Trump
+      "NonTrump" -> Just NonTrump
+      _ -> Nothing
+
+unsafeReadRegime :: String -> Regime
+unsafeReadRegime s = unsafePartial $ fromJust (read s) :: Regime
 
 lastYearKnown :: Regime -> Year
 lastYearKnown Trump = unsafeMakeYear 2022
