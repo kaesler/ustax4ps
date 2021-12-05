276320f0df0c59f34a396b7e36ea9279b10f6cf5
Kevin Esler <kevin.esler@gmail.com>
2021-12-05 16:50:36 -0500

    Progress on shim

--- src/GoogleSheetShim.js
+++ src/GoogleSheetShim.js
@@ -5,50 +5,51 @@ const Trump = M.unsafeReadRegime('Trump');
 const year2021 = M.unsafeMakeYear(2021);
 const birthDate = M.unsafeMakeDate(1955)(10)(2);
 const personalExemptions = 1;
+const partiallyBoundRegime = M.bindRegime(Trump)(year2021)(birthDate);
 
-function STD_DEDUCTION(filingStatus) {
-  
-  const fs = M.unsafeReadFilingStatus(filingStatus);
-    
-  const boundRegime = M.bindRegime(Trump)(year2021)(birthDate)(fs)(1); 
+function bindFilingStatus(filingStatusName) {
+  const filingStatus = M.unsafeReadFilingStatus(filingStatusName);
+
+  return partiallyBoundRegime(filingStatus)(personalExemptions);
+}
+
+function STD_DEDUCTION(filingStatusName) {
+  const boundRegime = bindFilingStatus(filingStatusName);
+
   return M.standardDeduction(boundRegime);
 }
 
-function BRACKET_START(filingStatus, rate) {
-  const T = PS.Taxes;
-  const fs = T.unsafeReadFilingStatus(filingStatus);
-  const r = T.unsafeOrdinaryRateFromNumber(rate);
+function BRACKET_START(filingStatusName, ordinaryRate) {
+  const br = bindFilingStatus(filingStatusName);
+  const brackets = br.ordinaryIncomeBrackets;
 
-  return T.ordinaryIncomeBracketStart(fs)(r);
+  return M.ordinaryIncomeBracketStart(brackets)(ordinaryRate);
 }
 
-function BRACKET_WIDTH(filingStatus, rate) {
-  const T = PS.Taxes;
-  const fs = T.unsafeReadFilingStatus(filingStatus);
-  const r = T.unsafeOrdinaryRateFromNumber(rate);
+function BRACKET_WIDTH(filingStatusName, ordinaryRate) {
+  const br = bindFilingStatus(filingStatusName);
+  const brackets = br.ordinaryIncomeBrackets;
 
-  return T.ordinaryIncomeBracketWidth(fs)(r);
+  return M.ordinaryIncomeBracketWidth(brackets)(ordinaryRate);
 }
 
-function LTCG_TAX_START(filingStatus) {
-  const T = PS.Taxes;
-  const fs = T.unsafeReadFilingStatus(filingStatus);
+function LTCG_TAX_START(filingStatusName) {
+  const br = bindFilingStatus(filingStatusName);
+  const brackets = br.ordinaryIncomeBrackets;
 
-  return T.startOfNonZeroQualifiedRateBracket(fs);
+  return M.startOfNonZeroQualifiedRateBracket(br.qualifiedIncomeBrackets);
 }
 
 function RMD_FRACTION_FOR_AGE(age) {
-  const T = PS.Taxes;
-
-  return T.unsafeRmdFractionForAge(age);
+  return M.unsafeRmdFractionForAge(age);
 }
 
-function FEDERAL_TAX_DUE(year, filingStatus, socSec, ordinaryIncomeNonSS, qualifiedIncome) {
-  const T = PS.Taxes;
-  const fs = T.unsafeReadFilingStatus(filingStatus);
+// TODO: Why year here? For taxable SS?
+function FEDERAL_TAX_DUE(year, filingStatusName, socSec, ordinaryIncomeNonSS, qualifiedIncome) {
+  const filingStatus = T.unsafeReadFilingStatus(filingStatusName);
 
   return T.federalTaxDue(year)(fs)(socSec)(ordinaryIncomeNonSS)(qualifiedIncome);
-  }
+}
 
 function MA_STATE_TAX_DUE(age, dependents, filingStatus, massacchusettsGrossIncome) {
   const T = PS.Taxes;
