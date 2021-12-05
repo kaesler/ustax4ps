86e411662adf9be64e0e719d752d751aa8961348
Kevin Esler <kevin.esler@gmail.com>
2021-12-05 18:24:39 -0500

    Polish the shim

--- src/GoogleSheetShim.js
+++ src/GoogleSheetShim.js
@@ -1,38 +1,43 @@
 // Note: This file must be loaded AFTER the PS bundle.
+// TODO: how to deal with years after 2022?
 const M = PS.GoogleSheetModule;
-const Trump = M.unsafeReadRegime('Trump');
-const year2021 = M.unsafeMakeYear(2021);
-const birthDate = M.unsafeMakeDate(1955)(10)(2);
-const personalExemptions = 1;
-const itemizedDeductions = 0;
-const partiallyBoundRegime = M.bindRegime(Trump)(year2021)(birthDate);
+const TheRegime = M.unsafeReadRegime('Trump');
+const TheBirthDate = M.unsafeMakeDate(1955)(10)(2);
+const ThePersonalExemptions = 1;
+const TheItemizedDeductions = 0;
 
-function bindFilingStatus(filingStatusName) {
+// For now use 2022 for future years.
+function predictIfNecessary(year) {
+  if (year <= 2022)
+    return year;
+  else
+    return 2022;
+}
+
+function bindRegime(year, filingStatusName) {
   const filingStatus = M.unsafeReadFilingStatus(filingStatusName);
-  return partiallyBoundRegime(filingStatus)(personalExemptions);
+  return M.bindRegime(TheRegime)(predictIfNecessary(year))(TheBirthDate)(filingStatus)(ThePersonalExemptions);
 }
 
-function STD_DEDUCTION(filingStatusName) {
-  const boundRegime = bindFilingStatus(filingStatusName); 
-  return M.standardDeduction(boundRegime);
+function STD_DEDUCTION(year, filingStatusName) {
+  const br = bindRegime(year, filingStatusName); 
+  return M.standardDeduction(br);
 }
 
-function BRACKET_START(filingStatusName, ordinaryRate) {
-  const br = bindFilingStatus(filingStatusName);
+function BRACKET_START(year, filingStatusName, ordinaryRate) {
+  const br = bindRegime(year, filingStatusName);
   const brackets = br.ordinaryIncomeBrackets;
   return M.ordinaryIncomeBracketStart(brackets)(ordinaryRate);
 }
 
-function BRACKET_WIDTH(filingStatusName, ordinaryRate) {
-  const br = bindFilingStatus(filingStatusName);
+function BRACKET_WIDTH(year, filingStatusName, ordinaryRate) {
+  const br = bindRegime(year, filingStatusName);
   const brackets = br.ordinaryIncomeBrackets;
   return M.ordinaryIncomeBracketWidth(brackets)(ordinaryRate);
 }
 
-function LTCG_TAX_START(filingStatusName) {
-  const br = bindFilingStatus(filingStatusName);
-  const brackets = br.ordinaryIncomeBrackets;
-
+function LTCG_TAX_START(year, filingStatusName) {
+  const br = bindRegime(year, filingStatusName);
   return M.startOfNonZeroQualifiedRateBracket(br.qualifiedIncomeBrackets);
 }
 
@@ -40,20 +45,19 @@ function RMD_FRACTION_FOR_AGE(age) {
   return M.unsafeRmdFractionForAge(age);
 }
 
-// TODO: Why year here? For taxable SS? Check it.
 function FEDERAL_TAX_DUE(year, filingStatusName, socSec, ordinaryIncomeNonSS, qualifiedIncome) {
   const filingStatus = M.unsafeReadFilingStatus(filingStatusName);
 
-  return M.taxDue(Trump)(year)(birthDate)(
-          filingStatus)(personalExemptions)(socSec)(
+  return M.taxDue(TheRegime)(predictIfNecessary(year))(TheBirthDate)(
+          filingStatus)(ThePersonalExemptions)(socSec)(
           ordinaryIncomeNonSS)(qualifiedIncome)(
-          itemizedDeductions);
+          TheItemizedDeductions);
 }
 
-function MA_STATE_TAX_DUE(age, dependents, filingStatusName, massacchusettsGrossIncome) {
+function MA_STATE_TAX_DUE(year, dependents, filingStatusName, massacchusettsGrossIncome) {
   const filingStatus = M.unsafeReadFilingStatus(filingStatusName);
  
-  return M.maStateTaxDue(year2021)(birthDate)(dependents)(filingStatus)(massacchusettsGrossIncome);
+  return M.maStateTaxDue(predictIfNecessary(year))(TheBirthDate)(dependents)(filingStatus)(massacchusettsGrossIncome);
 }
 
 function TAX_SLOPE(year, filingStatusName, socSec, ordinaryIncomeNonSS, qualifiedIncome) {
