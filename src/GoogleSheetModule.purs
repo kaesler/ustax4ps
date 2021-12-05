02a1837b5f188107ddc8e4493f5b63db5570d95c
Kevin Esler <kevin.esler@gmail.com>
2021-12-05 14:19:10 -0500

    Expose more

--- src/GoogleSheetModule.purs
+++ src/GoogleSheetModule.purs
@@ -7,14 +7,20 @@ module GoogleSheetModule
   , module Federal.RMDs
   , module Federal.TaxableSocialSecurity
   , module UnsafeDates
+  , showIt
   ) where
 
 -- Import here waht we want accessbible from GoogleSheetInterface
-import CommonTypes (FilingStatus(..))
+
+import Prelude
+import UnsafeDates (unsafeMakeDate, unsafeMakeDay, unsafeMakeMonth, unsafeMakeYear)
+import CommonTypes (FilingStatus(..), unsafeReadFilingStatus)
 import Federal.BoundRegime (BoundRegime(..), bindRegime, netDeduction, personalExemptionDeduction, standardDeduction)
 import Federal.OrdinaryIncome (ordinaryIncomeBracketStart, ordinaryIncomeBracketWidth)
 import Federal.QualifiedIncome (startOfNonZeroQualifiedRateBracket)
-import Federal.Regime (Regime(..), unsafeReadRegime)
 import Federal.RMDs (unsafeRmdFractionForAge)
+import Federal.Regime (Regime(..), unsafeReadRegime)
 import Federal.TaxableSocialSecurity (amountTaxableInflationAdjusted)
-import UnsafeDates
+
+showIt :: forall t. Show t => t -> String
+showIt x = show x
