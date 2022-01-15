43e68abc4162439bf8b27598e4d8b2b16b4eeff7
Kevin Esler <kevin.esler@gmail.com>
2022-01-15 15:17:36 -0500

    rateSuccessor

--- src/Brackets.purs
+++ src/Brackets.purs
@@ -9,12 +9,13 @@ module Brackets
   , taxableIncomeToEndOfBracket
   ) where
 
+import Data.Array as Array
 import Data.Maybe
 import Prelude
 import Data.Map (Map, keys)
 import Data.Map as Map
-import Data.Tuple (Tuple(..))
-import Moneys (IncomeThreshold, TaxPayable, TaxableIncome, makeFromInt)
+import Data.Tuple (Tuple(..), fst, snd)
+import Moneys (IncomeThreshold, TaxPayable, TaxableIncome, inflateThreshold, makeFromInt)
 import TaxRate (class TaxRate)
 import Undefined (undefined)
 
@@ -30,10 +31,20 @@ fromPairs tuples mkRate =
     Map.fromFoldable $ map f tuples
 
 inflateThresholds :: forall r. TaxRate r => Number -> Brackets r -> Brackets r
-inflateThresholds = undefined
+inflateThresholds factor = map (inflateThreshold factor)
 
 rateSuccessor :: forall r. TaxRate r => r -> Brackets r -> Maybe r
-rateSuccessor = undefined
+rateSuccessor rate brackets =
+  let
+    rates = Array.fromFoldable $ keys brackets
+
+    ratesTail = Array.drop 1 rates
+
+    pairs = Array.zip rates ratesTail
+
+    pair = Array.find (\p -> fst p == rate) pairs
+  in
+    map snd pair
 
 ratesExceptTop :: forall r. TaxRate r => Brackets r -> Array r
 ratesExceptTop = undefined
