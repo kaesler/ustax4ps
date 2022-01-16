ad91808e69ea88fa944d57f5546debe984df495c
Kevin Esler <kevin.esler@gmail.com>
2022-01-16 18:05:36 -0500

    OrdinaryBrackets

---
+++ src/Federal/FederalTaxRate.purs
@@ -0,0 +1,28 @@
+module Federal.FederalTaxRate
+  ( FederalTaxRate
+  , mkFederalTaxRate
+  )
+  where
+
+import Prelude
+
+import TaxRate (class TaxRate )
+import Effect.Exception.Unsafe (unsafeThrow)
+import Data.Interpolate (i)
+import Math (abs)
+newtype FederalTaxRate = FederalTaxRate Number
+derive newtype instance Eq FederalTaxRate
+derive newtype instance Ord FederalTaxRate
+derive newtype instance Show FederalTaxRate
+
+mkFederalTaxRate :: Number -> FederalTaxRate
+mkFederalTaxRate d
+  | d < 0.0 = unsafeThrow $ i "Invalid FederalTaxRate " (show d)
+  | d > 0.9 = unsafeThrow $ i "Invalid FederalTaxRate " (show d)
+  | otherwise = FederalTaxRate d
+
+instance TaxRate FederalTaxRate where
+  zeroRate = mkFederalTaxRate 0.0
+  rateToNumber (FederalTaxRate d) = d
+  absoluteDifference (FederalTaxRate d1) (FederalTaxRate d2) =
+    FederalTaxRate (abs (d1 - d2))
---
+++ src/Federal/OrdinaryBrackets.purs
@@ -0,0 +1,49 @@
+module Federal.OrdinaryBrackets
+  ( OrdinaryBrackets
+  , fromPairs
+  , inflateThresholds
+  , ordinaryIncomeBracketWidth
+  , ordinaryRatesExceptTop
+  , rateSuccessor
+  , taxFunctionFor
+  , taxToEndOfOrdinaryBracket
+  , taxableIncomeToEndOfOrdinaryBracket
+  )
+  where
+  
+import Prelude
+
+import Brackets as Brackets
+import Data.Maybe (Maybe)
+import Data.Tuple (Tuple)
+import Federal.FederalTaxRate (FederalTaxRate, mkFederalTaxRate)
+import Moneys (TaxPayable, TaxableIncome)
+import Safe.Coerce (coerce)
+import TaxFunction (TaxFunction, bracketsTaxFunction)
+
+newtype OrdinaryBrackets = OrdinaryBrackets (Brackets.Brackets FederalTaxRate)
+derive newtype instance Show OrdinaryBrackets
+
+fromPairs :: Array (Tuple Number Int) -> OrdinaryBrackets
+fromPairs pairs = coerce $ Brackets.fromPairs pairs mkFederalTaxRate
+
+inflateThresholds :: Number -> OrdinaryBrackets -> OrdinaryBrackets
+inflateThresholds factor (OrdinaryBrackets brackets) = coerce $ Brackets.inflateThresholds factor brackets
+
+taxFunctionFor :: OrdinaryBrackets -> TaxFunction
+taxFunctionFor (OrdinaryBrackets brs) = bracketsTaxFunction brs
+
+rateSuccessor :: FederalTaxRate -> OrdinaryBrackets -> Maybe FederalTaxRate
+rateSuccessor rate brackets = coerce $ Brackets.rateSuccessor rate (coerce brackets)
+
+ordinaryRatesExceptTop :: OrdinaryBrackets -> Array FederalTaxRate
+ordinaryRatesExceptTop brackets = Brackets.ratesExceptTop (coerce brackets)
+
+taxableIncomeToEndOfOrdinaryBracket :: OrdinaryBrackets -> FederalTaxRate -> TaxableIncome
+taxableIncomeToEndOfOrdinaryBracket brackets = Brackets.taxableIncomeToEndOfBracket (coerce brackets)
+
+ordinaryIncomeBracketWidth :: OrdinaryBrackets -> FederalTaxRate -> TaxableIncome
+ordinaryIncomeBracketWidth brackets = Brackets.bracketWidth (coerce brackets)
+
+taxToEndOfOrdinaryBracket :: OrdinaryBrackets -> FederalTaxRate -> TaxPayable
+taxToEndOfOrdinaryBracket brackets = Brackets.taxToEndOfBracket (coerce brackets)
