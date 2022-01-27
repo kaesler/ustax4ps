67376db7f9f7dcaf2564c0156491f5de2a0eef65
Kevin Esler <kevin.esler@gmail.com>
2022-01-27 14:12:53 -0500

    Reinstate test

--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -3,10 +3,11 @@ module Test.Main where
 import AgeSpec as AgeSpec
 import Effect (Effect)
 import GoldenTestsAgainstScalaImpl as Golden
+import Federal.OrdinaryBracketSpec as OrdinaryBracketSpec
 import Prelude
 
 main :: Effect Unit
 main = do
-  --OrdinaryIncomeBracketSpec.runAllTests
+  OrdinaryBracketSpec.runAllTests
   Golden.runAllTests
   AgeSpec.runAllTests
