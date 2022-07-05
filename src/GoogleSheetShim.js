25d78a32d742b333c9924609612aa036f05faf4d
Kevin Esler <kevin.esler@gmail.com>
2022-07-05 14:53:35 -0400

    Added JsDoc.

--- src/GoogleSheetShim.js
+++ src/GoogleSheetShim.js
@@ -23,11 +23,30 @@ function bindRegime(yearAsNumber, filingStatusName) {
   );
 }
 
+/**
+ * Standard deduction for a given year and filing status.
+ * Example: STD_DEDUCTION(2022, 'HeadOfHousehold')
+ *
+ * @param {number} yearAsNumber 
+ * @param {string} filingStatusName 
+ * @returns The standard deduction
+ * @customfunction
+ */
 function STD_DEDUCTION(yearAsNumber, filingStatusName) {
   const br = bindRegime(yearAsNumber, filingStatusName); 
   return M.standardDeduction(br);
 }
 
+/**
+ * Width of an ordinary income tax bracket.
+ * Example: BRACKET_WIDTH(2022, 'Single', 10)
+ * 
+ * @param {number} yearAsNumber 
+ * @param {string} filingStatusName 
+ * @param {number} ordinaryRatePercentage 
+ * @returns The width of the specified bracket.
+ * @customfunction
+ */
 function BRACKET_WIDTH(yearAsNumber, filingStatusName, ordinaryRatePercentage) {
   const br = bindRegime(yearAsNumber, filingStatusName);
   const brackets = br.ordinaryBrackets;
@@ -35,15 +54,44 @@ function BRACKET_WIDTH(yearAsNumber, filingStatusName, ordinaryRatePercentage) {
   return M.ordinaryIncomeBracketWidth(brackets)(rate);
 }
 
+/**
+ * Threshold above which long term capital gains are taxed.
+ * Example: LTCG_TAX_START(2022, 'HeadOfHousehold')
+ * 
+ * @param {number} yearAsNumber 
+ * @param {string} filingStatusName 
+ * @returns the taxable income threshold.
+ * @customfunction
+ */
 function LTCG_TAX_START(yearAsNumber, filingStatusName) {
   const br = bindRegime(yearAsNumber, filingStatusName);
   return M.startOfNonZeroQualifiedRateBracket(br.qualifiedBrackets);
 }
 
+/**
+ * The RMD fraction for a given age.
+ * Example: RMD_FRACTION_FOR_AGE(76)
+ * 
+ * @param {number} age
+ * @returns the RMD fraction
+ * @customfunction
+ */
 function RMD_FRACTION_FOR_AGE(age) {
   return M.unsafeRmdFractionForAge(age);
 }
 
+/**
+ * The Federal tax due.
+ * Example: FEDERAL_TAX_DUE(2022, 'Single', 10000, 40000, 5000)
+ * 
+ * @param {number} yearAsNumber 
+ * @param {string} filingStatusName 
+ * @param {number} socSec 
+ * @param {number} ordinaryIncomeNonSS 
+ * @param {number} qualifiedIncome 
+ * @returns the Federal tax due
+ * @customfunction
+ */
 function FEDERAL_TAX_DUE(yearAsNumber, filingStatusName, socSec, ordinaryIncomeNonSS, qualifiedIncome) {
   const filingStatus = M.unsafeReadFilingStatus(filingStatusName);
  
@@ -58,6 +106,17 @@ function FEDERAL_TAX_DUE(yearAsNumber, filingStatusName, socSec, ordinaryIncomeN
     TheItemizedDeductions);
 }
 
+/**
+ * The MA state income tax due.
+ * Example: MA_STATE_TAX_DUE(2022, 1, 'Married', 130000)
+ * 
+ * @param {number} yearAsNumber 
+ * @param {number} dependents 
+ * @param {string} filingStatusName 
+ * @param {number} massachusettsGrossIncome 
+ * @returns the MA state income tax due.
+ * @customfunction
+ */
 function MA_STATE_TAX_DUE(yearAsNumber, dependents, filingStatusName, massachusettsGrossIncome) {
   const filingStatus = M.unsafeReadFilingStatus(filingStatusName);
  
@@ -69,6 +128,18 @@ function MA_STATE_TAX_DUE(yearAsNumber, dependents, filingStatusName, massachuse
     massachusettsGrossIncome);
 }
 
+/**
+ * The marginal tax rate.
+ * Example: TAX_SLOPE(2022, 'Single', 10000, 40000, 5000)
+ * 
+ * @param {number} yearAsNumber 
+ * @param {string} filingStatusName 
+ * @param {number} socSec 
+ * @param {number} ordinaryIncomeNonSS 
+ * @param {number} qualifiedIncome 
+ * @returns the marginal tax rate.
+ * @customfunction
+ */
 function TAX_SLOPE(yearAsNumber, filingStatusName, socSec, ordinaryIncomeNonSS, qualifiedIncome) {
     const deltaX = 1000.0
 
@@ -92,12 +163,33 @@ function TAX_SLOPE(yearAsNumber, filingStatusName, socSec, ordinaryIncomeNonSS, 
   return deltaY/deltaX;
 }
 
+/**
+ * The amount of Social Security income that is taxable.
+ * Example: TAXABLE_SS('HeadOfHousehold', 20000, 52000)
+ * 
+ * @param {string} filingStatusName 
+ * @param {number} ssRelevantOtherIncome 
+ * @param {number} socSec 
+ * @returns the amount of Social Security income that is taxable
+ * @customfunction
+ */
 function TAXABLE_SS(filingStatusName, ssRelevantOtherIncome, socSec) {
   const filingStatus = M.unsafeReadFilingStatus(filingStatusName);
 
   return M.amountTaxable(filingStatus)(socSec)(ssRelevantOtherIncome);
 }
 
+/**
+ * The amount of Social Security income that is taxable, adjusted for inflation.
+ * Example: TAXABLE_SS_ADJUSTED(2030, 'HeadOfHousehold', 20000, 52000)
+ * 
+ * @param {number} yearAsNumber 
+ * @param {string} filingStatusName 
+ * @param {number} ssRelevantOtherIncome 
+ * @param {number} socSec 
+ * @returns the amount of Social Security income that is taxable, adjusted for inflation.
+ * @customfunction
+ */
 function TAXABLE_SS_ADJUSTED(yearAsNumber, filingStatusName, ssRelevantOtherIncome, socSec) {
   const year = M.unsafeMakeYear(yearAsNumber);
   const filingStatus = M.unsafeReadFilingStatus(filingStatusName);
