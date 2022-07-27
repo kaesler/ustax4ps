32b922fc47dfb7be5a229cc6c85ac2df1af3eaf9
Kevin Esler <kevin.esler@gmail.com>
2022-07-27 13:32:49 -0400

    Golden tests for future years pass now

--- test/Test.Main.purs
+++ test/Test.Main.purs
@@ -1,15 +1,17 @@
 module Test.Main where
 
+import Prelude
 import AgeSpec as AgeSpec
 import Effect (Effect)
+import Federal.OrdinaryBracketSpec as OrdinaryBracketSpec
+import Federal.Yearly.YearlyValuesSpec as YearlyValuesSpec
 import FutureYearsGoldenTestsAgainstScalaImpl as FutureYearsGolden
 import KnownYearsGoldenTestsAgainstScalaImpl as KnownYearsGolden
-import Federal.OrdinaryBracketSpec as OrdinaryBracketSpec
-import Prelude
 
 main :: Effect Unit
 main = do
   OrdinaryBracketSpec.runAllTests
+  YearlyValuesSpec.runAllTests
   FutureYearsGolden.runAllTests
   KnownYearsGolden.runAllTests
   AgeSpec.runAllTests
