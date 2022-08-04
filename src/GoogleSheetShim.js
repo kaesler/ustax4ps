f3b7c6969b12062f33365929d58db4422855bc4e
Kevin Esler <kevin.esler@gmail.com>
2022-08-04 15:03:03 -0400

    Adopt KTL_ prefix in shim API.

--- src/GoogleSheetShim.js
+++ src/GoogleSheetShim.js
@@ -1,34 +1,8 @@
 // Note: This file must be loaded AFTER the code compiled from Purescript.
 
-// For now use 2022 for future years.
-// TODO: this will have to go.
-function use2022after2022(yearAsNumber) {
-  if (yearAsNumber <= 2022)
-    return unsafeMakeYear(yearAsNumber);
-  else
-    return unsafeMakeYear(2022);
-}
-
-function bindRegime(yearAsNumber, filingStatusName) {
-  const filingStatus = unsafeReadFilingStatus(filingStatusName);
-
-  return boundRegimeForKnownYear(use2022after2022(yearAsNumber))(filingStatus);
-}
-
-function toPurescriptDate(dateObject) {
-  if (typeof(dateObject) != "object")
-    throw "Date object required";
-  if (! dateObject instanceof Date)
-    throw "Date object required";
-  const year = 1900 + dateObject.getYear();
-  const month = 1 + dateObject.getMonth();
-  const dayOfMonth = dateObject.getDate();
-  return unsafeMakeDate(year)(month)(dayOfMonth);
-}
-
 /**
  * Standard deduction for a given year and filing status.
- * Example: STD_DEDUCTION(2022, 'HeadOfHousehold')
+ * Example: KTL_STD_DEDUCTION(2022, 'HeadOfHousehold')
  *
  * @param {number} yearAsNumber 
  * @param {string} filingStatusName 
@@ -36,7 +10,7 @@ function toPurescriptDate(dateObject) {
  * @returns The standard deduction
  * @customfunction
  */
-function STD_DEDUCTION(yearAsNumber, filingStatusName, birthDateAsObject) {
+function KTL_STD_DEDUCTION(yearAsNumber, filingStatusName, birthDateAsObject) {
   const br = bindRegime(yearAsNumber, filingStatusName); 
   const birthDate = toPurescriptDate(birthDateAsObject);
 
@@ -45,7 +19,7 @@ function STD_DEDUCTION(yearAsNumber, filingStatusName, birthDateAsObject) {
 
 /**
  * Width of an ordinary income tax bracket.
- * Example: BRACKET_WIDTH(2022, 'Single', 10)
+ * Example: KTL_BRACKET_WIDTH(2022, 'Single', 10)
  * 
  * @param {number} yearAsNumber 
  * @param {string} filingStatusName 
@@ -53,7 +27,7 @@ function STD_DEDUCTION(yearAsNumber, filingStatusName, birthDateAsObject) {
  * @returns The width of the specified bracket.
  * @customfunction
  */
-function BRACKET_WIDTH(yearAsNumber, filingStatusName, ordinaryRatePercentage) {
+function KTL_BRACKET_WIDTH(yearAsNumber, filingStatusName, ordinaryRatePercentage) {
   const br = bindRegime(yearAsNumber, filingStatusName);
   const brackets = br.ordinaryBrackets;
   const rate = ordinaryRatePercentage / 100.0;
@@ -62,33 +36,33 @@ function BRACKET_WIDTH(yearAsNumber, filingStatusName, ordinaryRatePercentage) {
 
 /**
  * Threshold above which long term capital gains are taxed.
- * Example: LTCG_TAX_START(2022, 'HeadOfHousehold')
+ * Example: KTL_LTCG_TAX_START(2022, 'HeadOfHousehold')
  * 
  * @param {number} yearAsNumber 
  * @param {string} filingStatusName 
  * @returns the taxable income threshold.
  * @customfunction
  */
-function LTCG_TAX_START(yearAsNumber, filingStatusName) {
+function KTL_LTCG_TAX_START(yearAsNumber, filingStatusName) {
   const br = bindRegime(yearAsNumber, filingStatusName);
   return startOfNonZeroQualifiedRateBracket(br.qualifiedBrackets);
 }
 
 /**
  * The RMD fraction for a given age.
- * Example: RMD_FRACTION_FOR_AGE(76)
+ * Example: KTL_RMD_FRACTION_FOR_AGE(76)
  * 
  * @param {number} age
  * @returns the RMD fraction
  * @customfunction
  */
-function RMD_FRACTION_FOR_AGE(age) {
+function KTL_RMD_FRACTION_FOR_AGE(age) {
   return unsafeRmdFractionForAge(age);
 }
 
 /**
  * The Federal tax due.
- * Example: FEDERAL_TAX_DUE(2022, 'Single', 10000, 40000, 5000)
+ * Example: KTL_FEDERAL_TAX_DUE(2022, 'Single', 10000, 40000, 5000)
  * 
  * @param {number} yearAsNumber 
  * @param {string} filingStatusName 
@@ -101,7 +75,7 @@ function RMD_FRACTION_FOR_AGE(age) {
  * @returns the Federal tax due
  * @customfunction
  */
-function FEDERAL_TAX_DUE(
+function KTL_FEDERAL_TAX_DUE(
   yearAsNumber, 
   filingStatusName, 
   birthDateAsObject,
@@ -126,8 +100,76 @@ function FEDERAL_TAX_DUE(
 }
 
 /**
+ * The marginal tax rate.
+ * Example: KTL_FEDERAL_TAX_SLOPE(2022, 'Single', 10000, 40000, 5000)
+ * 
+ * @param {number} yearAsNumber 
+ * @param {string} filingStatusName 
+ * @param {object} birthDateAsObject
+ * @param {number} personalExemptions
+ * @param {number} socSec 
+ * @param {number} ordinaryIncomeNonSS 
+ * @param {number} qualifiedIncome 
+ * @param {number} itemizedDeductions 
+ * @returns the marginal tax rate.
+ * @customfunction
+ */
+function KTL_FEDERAL_TAX_SLOPE(
+  yearAsNumber, 
+  filingStatusName, 
+  birthDateAsObject, 
+  personalExemptions,
+  socSec, 
+  ordinaryIncomeNonSS, 
+  qualifiedIncome,
+  itemizedDeductions
+  ) {
+    const deltaX = 1000.0
+
+  const federalTaxAtStart = KTL_FEDERAL_TAX_DUE(
+    yearAsNumber, 
+    filingStatusName, 
+    birthDateAsObject,
+    personalExemptions,
+    socSec, 
+    ordinaryIncomeNonSS, 
+    qualifiedIncome,
+    itemizedDeductions
+  );
+  const federalTaxAtEnd = KTL_FEDERAL_TAX_DUE(
+    yearAsNumber, 
+    filingStatusName, 
+    birthDateAsObject,
+    personalExemptions,
+    socSec, 
+    ordinaryIncomeNonSS + deltaX, 
+    qualifiedIncome,
+    itemizedDeductions
+  );
+  const deltaY = (federalTaxAtEnd - federalTaxAtStart);
+
+  return deltaY/deltaX;
+}
+
+/**
+ * The amount of Social Security income that is taxable.
+ * Example: KTL_TAXABLE_SOCIAL_SECURITY('HeadOfHousehold', 20000, 52000)
+ * 
+ * @param {string} filingStatusName 
+ * @param {number} ssRelevantOtherIncome 
+ * @param {number} socSec 
+ * @returns the amount of Social Security income that is taxable
+ * @customfunction
+ */
+function KTL_TAXABLE_SOCIAL_SECURITY(filingStatusName, ssRelevantOtherIncome, socSec) {
+  const filingStatus = unsafeReadFilingStatus(filingStatusName);
+
+  return amountTaxable(filingStatus)(socSec)(ssRelevantOtherIncome);
+}
+
+/**
  * The MA state income tax due.
- * Example: MA_STATE_TAX_DUE(2022, 1, 'Married', 130000)
+ * Example: KTL_MA_STATE_TAX_DUE(2022, 1, 'Married', 130000)
  * 
  * @param {number} yearAsNumber 
  * @param {string} filingStatusName 
@@ -137,7 +179,7 @@ function FEDERAL_TAX_DUE(
  * @returns the MA state income tax due.
  * @customfunction
  */
-function MA_STATE_TAX_DUE(
+ function KTL_MA_STATE_TAX_DUE(
   yearAsNumber, 
   filingStatusName, 
   birthDateAsObject, 
@@ -155,96 +197,28 @@ function MA_STATE_TAX_DUE(
     massachusettsGrossIncome);
 }
 
-// TODO: eliminate?
-/**
- * The marginal tax rate.
- * Example: TAX_SLOPE(2022, 'Single', 10000, 40000, 5000)
- * 
- * @param {number} yearAsNumber 
- * @param {string} filingStatusName 
- * @param {object} birthDateAsObject
- * @param {number} personalExemptions
- * @param {number} socSec 
- * @param {number} ordinaryIncomeNonSS 
- * @param {number} qualifiedIncome 
- * @param {number} itemizedDeductions 
- * @returns the marginal tax rate.
- * @customfunction
- */
-function TAX_SLOPE(
-  yearAsNumber, 
-  filingStatusName, 
-  birthDateAsObject, 
-  personalExemptions,
-  socSec, 
-  ordinaryIncomeNonSS, 
-  qualifiedIncome,
-  itemizedDeductions
-  ) {
-    const deltaX = 1000.0
-
-  const federalTaxAtStart = FEDERAL_TAX_DUE(
-    yearAsNumber, 
-    filingStatusName, 
-    birthDateAsObject,
-    personalExemptions,
-    socSec, 
-    ordinaryIncomeNonSS, 
-    qualifiedIncome,
-    itemizedDeductions
-  );
-  const federalTaxAtEnd = FEDERAL_TAX_DUE(
-    yearAsNumber, 
-    filingStatusName, 
-    birthDateAsObject,
-    personalExemptions,
-    socSec, 
-    ordinaryIncomeNonSS + deltaX, 
-    qualifiedIncome,
-    itemizedDeductions
-  );
-  const deltaY = (federalTaxAtEnd - federalTaxAtStart) + 
-    (deltaX * maStateTaxRate(unsafeMakeYear(yearAsNumber)));
-
-  return deltaY/deltaX;
+// For now use 2022 for future years.
+// TODO: this will have to go.
+function use2022after2022(yearAsNumber) {
+  if (yearAsNumber <= 2022)
+    return unsafeMakeYear(yearAsNumber);
+  else
+    return unsafeMakeYear(2022);
 }
 
-/**
- * The amount of Social Security income that is taxable.
- * Example: TAXABLE_SS('HeadOfHousehold', 20000, 52000)
- * 
- * @param {string} filingStatusName 
- * @param {number} ssRelevantOtherIncome 
- * @param {number} socSec 
- * @returns the amount of Social Security income that is taxable
- * @customfunction
- */
-function TAXABLE_SOCIAL_SECURITY(filingStatusName, ssRelevantOtherIncome, socSec) {
+function bindRegime(yearAsNumber, filingStatusName) {
   const filingStatus = unsafeReadFilingStatus(filingStatusName);
 
-  return amountTaxable(filingStatus)(socSec)(ssRelevantOtherIncome);
+  return boundRegimeForKnownYear(use2022after2022(yearAsNumber))(filingStatus);
 }
 
-// TODO: eliminate
-/**
- * The amount of Social Security income that is taxable, adjusted for inflation.
- * Example: TAXABLE_SS_ADJUSTED(2030, 'HeadOfHousehold', 20000, 52000)
- * 
- * @param {number} yearAsNumber 
- * @param {string} filingStatusName 
- * @param {number} ssRelevantOtherIncome 
- * @param {number} socSec 
- * @returns the amount of Social Security income that is taxable, adjusted for inflation.
- * @customfunction
- */
-function TAXABLE_SS_ADJUSTED(yearAsNumber, filingStatusName, ssRelevantOtherIncome, socSec) {
-  const year = unsafeMakeYear(yearAsNumber);
-  const filingStatus = unsafeReadFilingStatus(filingStatusName);
-
-  return amountTaxableInflationAdjusted(
-    year)(
-    filingStatus)(
-    socSec)(
-    ssRelevantOtherIncome
-  );
+function toPurescriptDate(dateObject) {
+  if (typeof(dateObject) != "object")
+    throw "Date object required";
+  if (! dateObject instanceof Date)
+    throw "Date object required";
+  const year = 1900 + dateObject.getYear();
+  const month = 1 + dateObject.getMonth();
+  const dayOfMonth = dateObject.getDate();
+  return unsafeMakeDate(year)(month)(dayOfMonth);
 }
