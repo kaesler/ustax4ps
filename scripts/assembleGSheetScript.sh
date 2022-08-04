fcbc675a3ed5d3b7e403e42a0ef5f5bf9355f944
Kevin Esler <kevin.esler@gmail.com>
2022-08-04 06:22:26 -0400

    Remove unnecessary script

--- scripts/assembleGSheetScript.sh
+++ scripts/assembleGSheetScript.sh
@@ -1,6 +1,9 @@
-./scripts/bundleGoogleSheetModule.sh
+spago bundle-module --main GoogleSheetModule --to dist/GoogleSheetModule.js
+# Remove the exports section.
 ./scripts/editGoogleSheetModule.sc <dist/GoogleSheetModule.js >dist/GSheetScript.js
 rm -f dist/GoogleSheetModule.js
+
+# Append the shim
 cat src/GoogleSheetShim.js >> dist/GSheetScript.js
 
 
